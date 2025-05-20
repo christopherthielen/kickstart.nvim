cb161c5e63568bc8c66c485a022da42f725d7346
Chris Thielen <cthielen@netflix.com>
2025-05-20 11:12:09 -1000

    My Prefs

---
+++ lua/custom/plugins/mason.lua
@@ -0,0 +1,21 @@
+require('mason').setup()
+require('mason-lspconfig').setup {
+  -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+  ensure_installed = {
+    'bashls', -- "bash-language-server",
+    'cssls', -- "css-lsp",
+    'cssmodules_ls', -- "cssmodules-language-server",
+    'docker_compose_language_service', -- "docker-compose-language-service",
+    'dockerls', -- "dockerfile-language-server",
+    'eslint-lsp',
+    'gradle_ls', -- "gradle-language-server",
+    'graphql', -- "graphql-language-service-cli",
+    'html', -- "html-lsp",
+    'jqls', -- "jq-lsp",
+    'jsonls', -- "json-lsp",
+    'lua_ls', -- "lua-language-server",
+    'ts_ls', -- "typescript-language-server",
+    'yamlls', -- yaml-language-server
+  },
+}
+return {}
