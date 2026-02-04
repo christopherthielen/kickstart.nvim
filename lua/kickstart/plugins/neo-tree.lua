f8854649cbcd40527e952edd5c68f58c447f510b
Chris Thielen <github@sandgnat.com>
2026-02-04 09:28:31 -1000

    Fix neo-tree auto-open on startup

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,8 +10,16 @@ return {
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
+    -- Open Neo-tree if Neovim was started without a file argument
+    if vim.fn.argc() == 0 and vim.fn.line2byte(vim.fn.line('$')) == -1 then
+      vim.cmd('Neotree show')
+    end
+  end,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
     filesystem = {
