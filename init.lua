48c2f839020bbf929a4415503bc38d3fd155b4ff
Chris Thielen <github@sandgnat.com>
2026-02-04 13:54:09 -1000

    Enable lazy.nvim update checker
    Automatically checks for plugin updates daily and notifies when available.
    Run :Lazy update to apply updates.

--- init.lua
+++ init.lua
@@ -892,6 +892,11 @@ require('lazy').setup({
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
+  checker = {
+    enabled = true,
+    notify = true,
+    frequency = 86400, -- check once per day (seconds)
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
